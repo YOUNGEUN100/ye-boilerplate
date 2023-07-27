28fd50249fd366f54fe0d31a0ee90efe9eec84ca
YOUNGEUN <westzerosilver@gmail.com>
2023-07-27 15:05:00 +0900

    실제 배포용 jar 파일 생성

--- homepage/src/layout/MainNavi.jsx
+++ homepage/src/layout/MainNavi.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { Link } from 'react-router-dom';
 
 function MainNavi() {
   return (
@@ -9,14 +10,14 @@ function MainNavi() {
     <Container>
       <Navbar.Brand href="#home">LOCO</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="me-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
+      <Navbar.Collapse id="basic-navbar-nav" className='collapse navbar-collapse justify-content-between'>
+        <Nav className="navbar-nav">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/about">About111</Nav.Link>
         </Nav>
-        <Nav className="me-auto">
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/signup">Signup</Nav.Link>
+        <Nav className="navbar-nav">
+          <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>
