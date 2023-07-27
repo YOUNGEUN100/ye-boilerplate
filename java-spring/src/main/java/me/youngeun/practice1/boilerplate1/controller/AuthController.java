a28c8c5a5575c77f82a393ffaec7f8175a8f9164
YOUNGEUN <westzerosilver@gmail.com>
2023-07-27 10:46:54 +0900

    cloud setting

--- java-spring/src/main/java/me/youngeun/practice1/boilerplate1/controller/AuthController.java
+++ java-spring/src/main/java/me/youngeun/practice1/boilerplate1/controller/AuthController.java
@@ -8,10 +8,7 @@ import me.youngeun.practice1.boilerplate1.model.dto.MemberResponseDto;
 import me.youngeun.practice1.boilerplate1.service.AuthService;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -19,6 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+    @GetMapping("/gettest1")
+    public ResponseEntity<String> gettest1() {
+        return ResponseEntity.ok("성공");
+    }
+
+
 
     @PostMapping("/signup")
     public ResponseEntity<MemberResponseDto> signup(@RequestBody MemberRequestDto requestDto) {
